1f1894fe050f474df2bc9ae9e0f9da06927720aa
Suwadith <suwadith.srithar@gmail.com>
2023-04-04 23:58:58 +0100

    Week 6

---
+++ 2. Programming with JavaScript/7. Creating arrays and objects/main.js
@@ -0,0 +1,22 @@
+var clothes = [];
+
+clothes.push("T-Shirts");
+clothes.push("Shorts");
+clothes.push("Shirts");
+clothes.push("Trousers");
+clothes.push("Hoodie");
+
+clothes.pop();
+
+clothes.push("Sweater");
+
+console.log(clothes[2]);
+
+
+
+var favCar = {};
+
+favCar.color = "Red";
+favCar.covertible  = true;
+
+console.log(favCar);
