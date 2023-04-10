84b8f2f3d36dd9e9417906a3ebe8daa80ac82d5c
Suwadith <suwadith.srithar@gmail.com>
2023-04-10 02:41:04 +0100

    Week 7

---
+++ 2. Programming with JavaScript/12. Array and object iteration/solution.js
@@ -0,0 +1,40 @@
+// Task 1
+var dairy = ['cheese', 'sour cream', 'milk', 'yogurt', 'ice cream', 'milkshake'];
+
+function logDairy() {
+    for (prod of Object.keys(dairy)) {
+        console.log(dairy[prod]);
+    }
+}
+
+logDairy();
+
+// Task 2
+const animal = {
+
+    canJump: true
+
+};
+
+const bird = Object.create(animal);
+
+bird.canFly = true;
+
+bird.hasFeathers = true;
+
+function birdCan() {
+    for (item of Object.keys(bird)) {
+        console.log(item + ": " + bird[item]);
+    }
+}
+
+birdCan();
+
+// Task 3
+function animalCan() {
+    for (item in bird) {
+        console.log(item + ": " + bird[item]);
+    }
+}
+
+animalCan();
