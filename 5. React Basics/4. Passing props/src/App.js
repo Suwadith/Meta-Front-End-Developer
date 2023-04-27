c9fb2af9834eb19c1c3724e316a2dd8ae49eebbf
Suwadith <suwadith.srithar@gmail.com>
2023-04-27 20:10:31 +0100

    update

---
+++ 5. React Basics/4. Passing props/src/App.js
@@ -0,0 +1,12 @@
+import Heading from "./Heading";
+
+function App() {
+    return (
+        <div className="App">
+            <Heading firstName="Damian" />
+            <Heading firstName="Matt" />
+        </div>
+    );
+};
+
+export default App;
